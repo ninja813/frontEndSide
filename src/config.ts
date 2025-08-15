89d38a4a9e1d37115bc119e1fba98a4f498b6e66
Chris-Donald <chrisdonaldwork@gmail.com>
2025-08-15 04:54:11 -0700

    version1.1

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
     // Replace with your deployed contract on chosen testnet
-    TOKEN_ADDRESS: "0xEb16FBE8757C8911Ba39baF6F34D6D34659ad0D2" as const,
+    TOKEN_ADDRESS: "0x781796EE7d925A823839BC990918c984BB864914" as const,
     // Default chain to suggest (Sepolia recommended)
     DEFAULT_CHAIN_ID: 11155111, // Sepolia
   };
